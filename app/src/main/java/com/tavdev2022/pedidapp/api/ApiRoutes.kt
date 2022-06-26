a100ca306649ce950a5f3180fe5a5578f4206fb2
Octavio Bustos Solis <tavitomijo90@gmail.com>
2022-06-26 00:06:45 -0500

    New Updated

--- app/src/main/java/com/tavdev2022/pedidapp/api/ApiRoutes.kt
+++ app/src/main/java/com/tavdev2022/pedidapp/api/ApiRoutes.kt
@@ -1,12 +1,11 @@
 package com.tavdev2022.pedidapp.api
 
 import com.tavdev2022.pedidapp.routes.UsersRoutes
-import retrofit2.Retrofit
 
 class ApiRoutes {
 
-    val API_URL = "http://192.168.12.243:3000/api/"
-    val retrofit = RetrofitClient()
+    private val API_URL = "http://192.168.12.243:3000/api/"
+    private val retrofit = RetrofitClient()
 
     fun getUsersRoutes(): UsersRoutes {
         return  retrofit.getClient(API_URL).create(UsersRoutes::class.java)
